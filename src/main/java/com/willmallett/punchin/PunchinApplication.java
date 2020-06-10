c64abb8a9b7393444738f1170c063cf9e9d11750
Will Mallett <mallett002@gmail.com>
2020-06-10 06:38:22 -0500

    first commit

---
+++ src/main/java/com/willmallett/punchin/PunchinApplication.java
@@ -0,0 +1,13 @@
+package com.willmallett.punchin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PunchinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PunchinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/willmallett/punchin/PunchinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willmallett.punchin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PunchinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
